b0362ea61e8b1f2a13ed29a32e58065c697fa31b
Navneet <navn0306@gmail.com>
2021-11-28 01:44:23 +0530

    ADD: Added welcome message for program

---
+++ src/com/bridge/LargestOfThreeNums/LargestOfThreeNums.java
@@ -0,0 +1,8 @@
+package com.bridge.LargestOfThreeNums;
+
+public class LargestOfThreeNums {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome, This program prints the largest of three numbers give as inputs");
+    }
+}
