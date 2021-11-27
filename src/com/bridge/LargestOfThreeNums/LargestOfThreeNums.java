41a205e2de817bdd6f8b7f1558c36fd2a07efa31
Navneet <navn0306@gmail.com>
2021-11-28 01:49:41 +0530

    Add: Added the functionality to compute the largest of three numbers

--- src/com/bridge/LargestOfThreeNums/LargestOfThreeNums.java
+++ src/com/bridge/LargestOfThreeNums/LargestOfThreeNums.java
@@ -1,8 +1,22 @@
 package com.bridge.LargestOfThreeNums;
 
+import java.util.Scanner;
+
 public class LargestOfThreeNums {
     public static void main(String[] args) {
+        int a, b, c, largest;
+//object of the Scanner class
+        Scanner sc = new Scanner(System.in);
+//reading input from the user
+        System.out.println("Enter the first number:");
+        a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        c = sc.nextInt();
 
-        System.out.println("\n Welcome, This program prints the largest of three numbers give as inputs");
+        largest = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+
+        System.out.println("The largest number is: " + largest);
     }
-}
+}  
